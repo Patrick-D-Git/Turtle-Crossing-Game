d0299f7b0e22645635659736a494db18ba18b725
Patrick D <patrick.montejo@gmail.com>
2024-02-25 10:01:02 -0500

    added cars class

---
+++ car.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+import random
+
+colors = ["red", "blue", "yellow", "green", "black", "orange", "violet", "pink"]
+
+
+class Car(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(1.5, 1)
+        self.color(random.choice(colors))
+        self.penup()
+        self.setheading(90)
+        self.goto(290, random.randint(-250, 250))
+
+    def move(self):
+        new_x = self.xcor() - 10
+        new_y = self.ycor()
+        self.goto(new_x, new_y)
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from turtle import Screen
 from player import Player
+from car import Car
 import time
 
 
 screen = Screen()
-screen.setup(width=800, height=600)
+screen.setup(width=800, height=800)
 screen.title("Turtle Crossing Game")
 screen.tracer(0)
 
@@ -15,9 +16,23 @@ screen.onkeypress(player.move_up, "Up")
 
 game_is_on = True
 
+cars = []
+cycle_count = 0
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    cycle_count += 1
+    if cycle_count == 5:
+        new_car = Car()
+        cars.append(new_car)
+        cycle_count = 0
+
+
+
+    for car in cars:
+        car.move()
+
 
     if player.ycor() > 280:
         player.increase_speed()
