1a79e0d053019b59633a51f2da5db22db3ac43af
Patrick D <patrick.montejo@gmail.com>
2024-02-25 14:45:45 -0500

    added starting and finish line visuals

--- board.py
+++ board.py
@@ -15,7 +15,10 @@ class Board(Turtle):
 
     def refresh(self):
         self.clear()
-        self.write(f"Level: {self.level}", align="center", font=("Courier", 25, "normal"))
+        self.starting_line()
+        self.finish_line()
+        self.goto(-335, 370)
+        self.write(f"Level: {self.level}", align="center", font=("Courier", 15, "normal"))
 
     def increase_level(self):
         self.level += 1
@@ -27,9 +30,21 @@ class Board(Turtle):
         self.write("Game Over", align="center", font=("Courier", 30, "normal"))
 
     def starting_line(self):
-        self.goto(400, -380)
         self.pencolor("black")
         self.pensize(2)
+        self.goto(400, -360)
         self.pendown()
-        self.goto(-400, -380)
+        self.goto(-400, -360)
         self.penup()
+        self.goto(320, -385)
+        self.write("Starting Line", align="center", font=("Courier", 15, "normal"))
+
+    def finish_line(self):
+        self.pencolor("black")
+        self.pensize(2)
+        self.goto(400, 360)
+        self.pendown()
+        self.goto(-400, 360)
+        self.penup()
+        self.goto(320, 370)
+        self.write("Finish Line", align="center", font=("Courier", 15, "normal"))
